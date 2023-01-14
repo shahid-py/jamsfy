582ec532324d5c2c1ff7d472a283686c7c840ebc
Mohd Shahid Khan <khan42290@gmail.com>
2023-01-14 10:46:42 +0530

    refactor

--- src/Categories/Home.jsx
+++ src/Categories/Home.jsx
@@ -15,14 +15,7 @@ const Home = ({ items, fetchProductByCategory,isLoading}) => {
         </a>
 
         <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-          <li class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white">
-            <button
-              className="  font-ibm text-sm"
-              onClick={() => fetchProductByCategory("all")}
-            >
-              All Products
-            </button>
-          </li>
+        
           <li class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
             
             <button onClick={() => fetchProductByCategory("women's clothing")}>
@@ -51,8 +44,8 @@ const Home = ({ items, fetchProductByCategory,isLoading}) => {
       </div>
 
       <div className="w-full bg-slate-50 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-8 gap-y-8 place-items-center px-4 py-3 mt-8 relative z-[5]">
-        {items &&
-          items.map(
+        {fetchProductByCategory &&
+          fetchProductByCategory.map(
             (item) =>  (<Item key={item.id} item={item} />)
           )}
       </div>
